1aa51c4770f3002df817d7319303d1e6cc758ab3
Jono7482 <JonoLaguna@Gmail.com>
2017-01-22 11:39:22 -0800

    Add files via upload

---
+++ GetSize.py
@@ -0,0 +1,148 @@
+
+import math
+
+
+def print_size(length, width, height, style):
+    facia = 4
+    top = 16
+    # Updraft
+    if style in 'Updraft':
+        # Front Bend Angle UD
+        frontbendtril = float((length - top) / 2)
+        frontbendhyp = math.sqrt((frontbendtril ** 2) + ((height - facia) ** 2))
+        frontbendangle = math.degrees(math.atan((height-facia) / frontbendtril))
+        print("Front Bend: " + str(int(90-frontbendangle)))
+
+        # Side Bend Angle UD
+        sidebendtril = float((width - top) / 2)
+        sidebendhyp = math.sqrt((sidebendtril ** 2) + ((height - facia) ** 2))
+        sidebendangle = math.degrees(math.atan((height-facia) / sidebendtril))
+        print("Side Bend: " + str(int(90-sidebendangle)))
+        print("UP Draft")
+
+        # Corner lengths
+        print("Front Face Length: " + str(frontbendhyp))
+        print("Sides Face Length: " + str(sidebendhyp))
+    # DownDraft
+    elif style in 'Downdraft':
+        # Front Bend Angle DD
+        ddfrontbendtril = float(length - top)
+        # ddfrontbendhyp = math.sqrt((ddfrontbendtril ** 2) + ((height - facia) ** 2))
+        ddfrontbendangle = math.degrees(math.atan((height-facia) / ddfrontbendtril))
+        print("Front Bend: " + str(int(90-ddfrontbendangle)))
+
+        # Side Bend Angle DD
+        ddsidebendtril = float((width - top) / 2)
+        # ddsidebendhyp = math.sqrt((ddsidebendtril ** 2) + ((height - facia) ** 2))
+        ddsidebendangle = math.degrees(math.atan((height-facia) / ddsidebendtril))
+        print("Side Bend: " + str(int(90-ddsidebendangle)))
+        print("Down Draft")
+    else:
+        print("You typed ", style, ".")
+        print("Must be UD or DD")
+
+
+def create_points(x, y, draft, face):
+    # number points starting 0 bottom left (0,0) going clockwise
+    # Updraft & Downdraft
+    facia = float(4)
+    top = float(16)
+
+    if (draft == "Updraft" or face == "Front") and (face != "Top" and face != "Hole"):
+        p0 = [0, 0]
+        p1 = [0, facia]
+        p2 = [(x-top)/2, y]
+        p3 = [(p2[0])+top, y]
+        p4 = [x, facia]
+        p5 = [x, 0]
+        points = p0, p1, p2, p3, p4, p5
+        return points
+    elif draft == "Downdraft" and face == "Side":
+        p0 = [0, 0]
+        p1 = [0, facia]
+        p2 = [0, y]
+        p3 = [top, y]
+        p4 = [x, facia]
+        p5 = [x, 0]
+        points = p0, p1, p2, p3, p4, p5
+        return points
+    elif face == "Top" or "Hole":
+        p0 = [0, 0]
+        p1 = [0, y]
+        p2 = [x, y]
+        p3 = [x, 0]
+
+        if draft == "Updraft":
+            p4 = [(x-top)/2, (y-top)/2]
+            p5 = [p4[0], p4[1]+top]
+            p6 = [p4[0]+top, p5[1]]
+            p7 = [p6[0], p4[1]]
+            points = p0, p1, p2, p3, p4, p5, p6, p7
+        elif draft == "Downdraft":
+            p4 = [(x-top)/2, y-top]
+            p5 = [p4[0], y]
+            p6 = [p4[0]+top, y]
+            p7 = [p6[0], p4[1]]
+            points = p0, p1, p2, p3, p4, p5, p6, p7
+        else:
+            print("Face is Top/Hole but invalid draft >", draft, "<")
+            return
+
+        if face == "Hole":
+            hole = 12
+            holeoffset = (top-hole)/2
+            h1 = [p5[0]+holeoffset, p5[1]-holeoffset]
+            h2 = [p7[0]-holeoffset, p7[1]+holeoffset]
+            points = h1, h2
+            return points
+        else:
+            return points
+
+    else:
+        print("Error in create_points style got >", draft, "< and >", face, "<")
+        return
+
+
+def find_scale(canvasx, canvasy, tpoints, height, offset):
+    # get length of longest X and Y lines
+    xlength = (tpoints[2][0] + tpoints[2][1]) + offset
+    ylength = tpoints[2][1] + height + offset
+
+    # Create scale on X or Y depending on what is larger
+    # in relation to canvas size
+    if canvasx / xlength <= canvasy / ylength:
+        scale = float(canvasx) / xlength
+    else:
+        scale = float(canvasy) / ylength
+
+    xlengthdif = (tpoints[2][0] - tpoints[2][1])
+    ylengthdif = tpoints[2][1] - height
+    lengthdif = xlengthdif, ylengthdif
+    return scale, lengthdif
+
+
+def locate_points_canvas(canvasx, canvasy, scale, points, face, offset, lengthdif):
+    # Scale points
+    lengthdifx = lengthdif[0] * (scale / 2)
+    lengthdify = lengthdif[1] * (scale / 2)
+    for n in range(len(points)):
+        points[n][0] = (points[n][0] * scale) + offset
+        points[n][1] = (points[n][1] * scale) + offset
+
+    # flip y axis put 0,0 in bottom left
+    for index in range(len(points)):
+        points[index][1] = canvasy - points[index][1]
+
+    # Front return points Side Move to bottom right then return points
+    if face is "front":
+        return points
+    elif face is "side":
+        for x in range(len(points)):
+            points[x][0] += (canvasx / 2) + lengthdifx
+        return points
+    elif face is "top" or "hole":
+        for x in range(len(points)):
+            points[x][1] -= (canvasy / 2) - lengthdify
+        return points
+    else:
+        print("locate_points_canvas face != front or side")
---
+++ Gui.py
@@ -0,0 +1,199 @@
+import random
+from tkinter import *
+from tkinter.ttk import Combobox
+
+import GetSize
+import Mesurements
+
+
+class Example(Frame):
+
+    def __init__(self, parent):
+        Frame.__init__(self, parent, background="#aaa")
+
+        self.parent = parent
+        self.parent.title("Jono's Kiln Hood Creator")
+        self.pack(fill=BOTH, expand=1)
+        self.centerwindow()
+
+    def centerwindow(self):
+
+        # Setting up Window
+        w = 800
+        h = 600
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+        xaxis = (sw - w) / 2
+        yaxis = (sh - h) / 2
+        self.parent.geometry('%dx%d+%d+%d' % (w, h, xaxis, yaxis))
+        # self.parent.overrideredirect(1)  # Remove border
+        # self.parent.attributes('-topmost', 1) # dunno
+        # self.parent.attributes("-fullscreen", True) #fullscreen
+
+    # Setting Grid
+        self.columnconfigure(1, weight=1)  # column 1 and 2 stretch for canvas
+        self.columnconfigure(2, weight=1)
+        # for x in range(0, 12): # pad all rows
+        #     self.rowconfigure(x, pad=1)
+        self.rowconfigure(10, weight=1)  # Last row of canvas stretches
+        self.rowconfigure(0, pad=8)
+        self.rowconfigure(11, pad=8)
+        self.rowconfigure(12, pad=8)
+
+    # canvas
+        canvas = Canvas(self, background="#fff")
+        canvas.grid(row=1, column=1, columnspan=2, rowspan=10, padx=4, sticky=N+W+E+S)
+
+    # input testing amounts
+        def setdef():
+            ltext.delete("1.0", END)
+            ltext.insert("1.0", "60.5")
+            wtext.delete("1.0", END)
+            wtext.insert("1.0", "52")
+            htext.delete("1.0", END)
+            htext.insert("1.0", "24")
+            stylecbox.set("Downdraft")
+
+        # time_loop(False)
+        def randsettings():
+            ltext.delete("1.0", END)
+            ltext.insert("1.0", random.randint(50, 100))
+            wtext.delete("1.0", END)
+            wtext.insert("1.0", random.randint(50, 100))
+            htext.delete("1.0", END)
+            htext.insert("1.0", random.randint(20, 60))
+            randdraft = "Downdraft", "Updraft"
+            stylecbox.set(randdraft[random.randint(0, 1)])
+            create()
+
+    # Create floats run getsize
+        def create():
+            canvasheight = canvas.winfo_height()
+            canvaswidth = canvas.winfo_width()
+
+            canvas.delete("all")
+            isfloat = bool(1)
+            lt = float
+            wt = float
+            ht = float
+            draft = stylecbox.get()
+
+            try:
+                lt = float(ltext.get("1.0", 'end-1c'))
+            except ValueError:
+                print("Length must be a number! ")
+                isfloat = False
+            try:
+                wt = float(wtext.get("1.0", 'end-1c'))
+            except ValueError:
+                print("Width must be a number!")
+                isfloat = False
+            try:
+                ht = float(htext.get("1.0", 'end-1c'))
+            except ValueError:
+                print("Height must be a number!")
+                isfloat = False
+
+            if isfloat:
+                # Get points
+                fpoints = GetSize.create_points(wt, ht, draft, "Front")
+                spoints = GetSize.create_points(lt, ht, draft, "Side")
+                tpoints = GetSize.create_points(wt, lt, draft, "Top")
+                hpoints = GetSize.create_points(wt, lt, draft, "Hole")
+
+                # Scale points to canvas
+                offset = 15
+
+                scale, lengthdif = GetSize.find_scale(canvaswidth, canvasheight, tpoints, ht, offset)
+
+                frontshape = GetSize.locate_points_canvas(
+                    canvaswidth, canvasheight, scale, fpoints, "front", offset, lengthdif)
+                sideshape = GetSize.locate_points_canvas(
+                    canvaswidth, canvasheight, scale, spoints, "side", offset, lengthdif)
+                topshape = GetSize.locate_points_canvas(
+                    canvaswidth, canvasheight, scale, tpoints, "top", offset, lengthdif)
+                holeshape = GetSize.locate_points_canvas(
+                    canvaswidth, canvasheight, scale, hpoints, "hole", offset, lengthdif)
+
+                # get measurement locations
+                frontcords = Mesurements.get_measurement_cords(frontshape)
+                sidecords = Mesurements.get_measurement_cords(sideshape)
+
+                # Get size arrays
+                frontsizearray = Mesurements.size_array(wt, ht)
+                sidesizearray = Mesurements.size_array(lt, ht)
+
+                # Print shapes to canvas
+                canvas.create_polygon(frontshape, fill="#ccc", outline="black", width=2)
+                canvas.create_polygon(sideshape, fill="#ccc", outline="black", width=2)
+                canvas.create_polygon(topshape[0:4], fill="#ccc", outline="black", width=2)
+                canvas.create_polygon(topshape[4:8], fill="#ccc", outline="black", width=2)
+                canvas.create_line(topshape[0], topshape[4], width=2)
+                canvas.create_line(topshape[1], topshape[5], width=2)
+                canvas.create_line(topshape[2], topshape[6], width=2)
+                canvas.create_line(topshape[3], topshape[7], width=2)
+                canvas.create_oval(holeshape, width=2, fill="#fff")
+
+                # print measurements to canvas
+                for n in range(0, 4):
+                    canvas_id = canvas.create_text(frontcords[n])
+                    canvas.itemconfig(canvas_id, text=frontsizearray[n])
+                for n in range(0, 4):
+                    canvas_id = canvas.create_text(sidecords[n])
+                    canvas.itemconfig(canvas_id, text=sidesizearray[n])
+                return
+
+            else:
+                return
+
+    # Frame objects
+        lbl = Label(self, text="Kiln Hoods Calculator", width=20)
+        lbl.grid(row=0, column=1, columnspan=2)
+        dbtn = Button(self, text="Defaults", width=10, command=setdef)
+        dbtn.grid(row=11, column=3, sticky=E)
+        hbtn = Button(self, text="Random", width=10, command=randsettings)
+        hbtn.grid(row=11, column=0)
+        quit_button = Button(self, text="Quit", width=10, command=self.quit)
+        quit_button.grid(row=12, column=3)
+
+        llbl = Label(self, text="Length:", width=10, background="#aaa")
+        llbl.grid(row=1, column=0, sticky=W+S)
+
+        ltext = Text(self, width=10, height=1)
+        ltext.insert(END, "Length")
+        ltext.grid(row=2, column=0)
+
+        wbl = Label(self, text="Width:", width=10, background="#aaa")
+        wbl.grid(row=3, column=0, sticky=W+S)
+
+        wtext = Text(self, width=10, height=1)
+        wtext.insert(END, "Width")
+        wtext.grid(row=4, column=0)
+
+        hlbl = Label(self, text="Height:", width=10, background="#aaa")
+        hlbl.grid(row=5, column=0, sticky=W+S)
+
+        htext = Text(self, width=10, height=1)
+        htext.insert(END, "Height")
+        htext.grid(row=6, column=0)
+
+        stylelbl = Label(self, text="Draft Type:", width=10, background="#aaa")
+        stylelbl.grid(row=7, column=0, sticky=W + S)
+
+        stylecboxvar = 0
+        stylecbox = Combobox(self, width=10, textvariable=stylecboxvar, state="readonly")
+        stylecbox['values'] = ('Updraft', 'Downdraft')
+        stylecbox.grid(row=8, column=0)
+
+        fakelbl = Label(self, text="    ", width=10, background="#aaa")
+        fakelbl.grid(row=9, column=0, sticky=W + S)
+
+        cbtn = Button(self, text="Create", width=10, command=create)
+        cbtn.grid(row=10, column=0, sticky=N)
+
+
+def main():
+
+    root = Tk()
+    Example(root)
+    root.mainloop()
