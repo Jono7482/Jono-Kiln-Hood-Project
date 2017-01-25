ab4dda98f650e0933b9bace4ffee586bf2b2af23
Jono7482 <JonoLaguna@Gmail.com>
2017-01-25 08:32:19 -0800

    Still working on ISO

--- Iso_Rotation.py
+++ Iso_Rotation.py
@@ -1,18 +1,31 @@
 from tkinter import *
-import math
+import GetSize
 
-c = Canvas(width=800, height=600)
+
+canvaswidth, canvasheight, canvasoffset = 800, 600, 15
+wt, lt, ht, draft, face = float(52), float(60.5), float(24), "Updraft", "Top"
+tpoints = GetSize.create_points(wt, lt, draft, "Top")
+scale, lengthdif = GetSize.find_scale(canvaswidth, canvasheight, tpoints, ht, canvasoffset)
+topshape = GetSize.locate_points_canvas(
+                    canvaswidth, canvasheight, scale, tpoints, "top", canvasoffset, lengthdif)
+print(topshape)
+
+
+c = Canvas(width=canvaswidth, height=canvasheight)
 c.pack()
 
 # a square
 # xy = [(50, 50), (150, 50), (150, 150), (50, 150)]
-xy = ([15.0, 465.0], [15.0, 446.3333333333333], [99.0, 353.0], [173.66666666666669, 353.0], [257.6666666666667, 446.3333333333333], [257.6666666666667, 465.0])
-print(xy)
-
+# xy = ([15.0, 465.0], [15.0, 446.3333333333333], [99.0, 353.0], [173.66666666666669, 353.0], [257.6666666666667, 446.3333333333333], [257.6666666666667, 465.0])
+# print(xy)
+xy = topshape[0:4]
 polygon_item = c.create_polygon(xy, fill="#ccc", outline="black", width=2)
+xy1 = topshape[4:8]
+polygon_item2 = c.create_polygon(xy1, fill="#ccc", outline="black", width=2)
 
-center = xy[0][0] + ((xy[5][0] - xy[0][0])/2), xy[2][1] + ((xy[0][1] - xy[2][1])/2)
 
+# center = xy[0][0] + ((xy[5][0] - xy[0][0])/2), xy[2][1] + ((xy[0][1] - xy[2][1])/2)
+center = xy[0][0] + ((xy[3][0] - xy[0][0])/2), xy[1][1] + ((xy[0][1] - xy[1][1])/2)
 
 print(center)
 
@@ -29,6 +42,7 @@ def press(event):
     global start
     start = getangle(event)
 
+
 def motion(event):
     # calculate current angle relative to initial angle
     global start
@@ -41,7 +55,17 @@ def motion(event):
         newxy.append(v.imag)
     c.coords(polygon_item, *newxy)
 
+    newxy1 = []
+    for x, y in xy1:
+        v = angle * (complex(x, y) - offset) + offset
+        newxy1.append(v.real)
+        newxy1.append(v.imag)
+    c.coords(polygon_item2, *newxy1)
+
+
+
 c.bind("<Button-1>", press)
 c.bind("<B1-Motion>", motion)
+
 
 mainloop()
