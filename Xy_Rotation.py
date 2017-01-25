3ca5ec33d6b8ec58c3e2ad3368666b1afa303cfa
Jono7482 <JonoLaguna@Gmail.com>
2017-01-25 15:31:52 -0800

    Still working on ISO

--- Xy_Rotation.py
+++ Xy_Rotation.py
@@ -8,26 +8,23 @@ tpoints = GetSize.create_points(wt, lt, draft, "Top")
 scale, lengthdif = GetSize.find_scale(canvaswidth, canvasheight, tpoints, ht, canvasoffset)
 topshape = GetSize.locate_points_canvas(
                     canvaswidth, canvasheight, scale, tpoints, "top", canvasoffset, lengthdif)
-print(topshape)
 
 
 c = Canvas(width=canvaswidth, height=canvasheight)
 c.pack()
 
-# a square
-# xy = [(50, 50), (150, 50), (150, 150), (50, 150)]
-# xy = ([15.0, 465.0], [15.0, 446.3333333333333], [99.0, 353.0], [173.66666666666669, 353.0], [257.6666666666667, 446.3333333333333], [257.6666666666667, 465.0])
-# print(xy)
-xy = topshape[0:4]
-polygon_item = c.create_polygon(xy, fill="#ccc", outline="black", width=2)
-xy1 = topshape[4:8]
-polygon_item2 = c.create_polygon(xy1, fill="#ccc", outline="black", width=2)
+xy = topshape
+polygon_item0 = c.create_polygon(xy[0:4], fill="#ccc", outline="black", width=2)
+# xy1 = topshape[4:8]
+polygon_item1 = c.create_polygon(xy[4:8], fill="#ccc", outline="black", width=2)
+print(xy)
+line_item0 = c.create_line(xy[0], xy[4], width=2)
+line_item1 = c.create_line(xy[1], xy[5], width=2)
+line_item2 = c.create_line(xy[2], xy[6], width=2)
+line_item3 = c.create_line(xy[3], xy[7], width=2)
 
-
-# center = xy[0][0] + ((xy[5][0] - xy[0][0])/2), xy[2][1] + ((xy[0][1] - xy[2][1])/2)
 center = xy[0][0] + ((xy[3][0] - xy[0][0])/2), xy[1][1] + ((xy[0][1] - xy[1][1])/2)
 
-print(center)
 
 def getangle(event):
     dx = c.canvasx(event.x) - center[0]
@@ -35,7 +32,8 @@ def getangle(event):
     try:
         return complex(dx, dy) / abs(complex(dx, dy))
     except ZeroDivisionError:
-        return 0.0 # cannot determine angle
+        return 0.0  # cannot determine angle
+
 
 def press(event):
     # calculate angle at start point
@@ -50,18 +48,17 @@ def motion(event):
     offset = complex(center[0], center[1])
     newxy = []
     for x, y in xy:
+        print(complex(x, y))
         v = angle * (complex(x, y) - offset) + offset
+        print(v)
         newxy.append(v.real)
         newxy.append(v.imag)
-    c.coords(polygon_item, *newxy)
-
-    newxy1 = []
-    for x, y in xy1:
-        v = angle * (complex(x, y) - offset) + offset
-        newxy1.append(v.real)
-        newxy1.append(v.imag)
-    c.coords(polygon_item2, *newxy1)
-
+    c.coords(polygon_item0, *newxy[0:8])
+    c.coords(polygon_item1, *newxy[8:16])
+    c.coords(line_item0, *newxy[0:2], *newxy[8:10])
+    c.coords(line_item1, *newxy[2:4], *newxy[10:12])
+    c.coords(line_item2, *newxy[4:6], *newxy[12:14])
+    c.coords(line_item3, *newxy[6:8], *newxy[14:16])
 
 
 c.bind("<Button-1>", press)
